1f1737fd6b1d05a2ebf3d61203076221aa42fa30
Ian Boston <ieb@tfd.co.uk>
2012-01-11 12:58:15 +1100

    Extracted Json -> DB loader configuration

---
+++ src/ShahnamaDJ/loaddb.py
@@ -0,0 +1,35 @@
+'''
+Created on Jan 10, 2012
+
+@author: ieb
+'''
+from ShahnamaDJ.records.models import Chapter, Country, Location, Manuscript,\
+    Scene, Illustration, Authority
+from ShahnamaDJ.content.models import Content
+
+# This the the mapping of folder into Object type for the json data
+# eg SOURCE_DATA/chapter contains json files for Chapter objects
+RECORDS_SOURCE_DATA = {
+        'chapter' : Chapter,
+        'country' : Country,
+        'location' : Location,
+        'manuscript' : Manuscript,
+        'scene' : Scene,
+        'illustration' : Illustration,
+        'ms-type' : Authority,
+        'ms-author' : Authority,
+        'ms-status' : Authority,
+        'ms-title' : Authority,
+        'ms-lang' : Authority,
+        'bib-class' : Authority,
+        'record-status' : Authority,
+        'chapter-k' : Authority,
+        'chapter-b' : Authority,
+        'chapter-ds' : Authority,
+        'ill-format' : Authority
+        }
+
+CONTENT_SOURCE_DATA = {
+        'content' : Content
+        }
+
